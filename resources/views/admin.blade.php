6fa6c7ee33e74a06d97404cec12d98fedebc07b1
Mark Jordan Ugtong <markjordan.ugtong.202200752@gmail.com>
2024-03-05 01:42:32 +0800

    Delete unnecessary image and CSS files.
    - Stablize Admin Page
    - Added Floating Button
    - Added Dark Mode and Light Mode Function (data cookies is not implemented)
    - Implemented SPC Font [might similar implemented (at admin dashboard)]

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,53 +1,302 @@
 <!DOCTYPE html>
-<html>
+<html lang="english">
   <head>
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
-    <link href="admin.css" rel="stylesheet" />
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="v379_198">
-      <div class="v379_212"></div>
-        <div class="v482_8437">
-          <div class="v461_984"></div>
-          <div class="v379_201"></div>
-          <div class="v482_8559"></div>
-          <span class="v379_210">Admin Dashboard</span>
-          <div class="v461_992"></div>
-          <div class="v461_973"></div>
-          <div class="v461_977"></div>
-          <div class="v461_978"></div>
-          <button class="v461_976">
-            <a href="https://example.com" target="_blank">STUDENTS</a>
-          </button>
-          <button class="v461_979">
-            <a href="https://example.com" target="_blank">INSTRUCTOR</a>
-          </button>
-          <button class="v461_980">
-            <a href="https://example.com" target="_blank">ATTENDANCE LOG</a>
-          </button>
-          <span class="v461_982">spcregistrar@gmail.com</span>
-          <span class="v461_985">(+63) 221-6246</span>
-          <div class="v461_987"></div>
-          <span class="v461_988">my.spc.edu.ph</span>
+    <title>Administrator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta property="twitter:card" content="summary_large_image" />
+
+    <style data-tag="reset-style-sheet">
+      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
+    </style>
+        <style data-tag="default-style-sheet">
+          html {
+            font-family: Inter;
+            font-size: 16px;
+          }
+
+          body {
+            font-weight: 400;
+            font-style:normal;
+            text-decoration: none;
+            text-transform: none;
+            letter-spacing: normal;
+            line-height: 1.15;
+            color: var(--dl-color-gray-black);
+            background-color: var(--dl-color-gray-black); // Change the background color to black
+
+          }
+        </style>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/animate.css@4.1.1/animate.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+          data-tag="font"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@teleporthq/teleport-custom-scripts/dist/style.css"
+        />
+      
+    <style>
+    .button-click-effect {
+      transition: transform 0.2s ease, background-color 0.2s ease;
+    }
+
+    .button-click-active {
+      transform: scale(0.95);
+      background-color: #e2e2e2; /* Example color, adjust as needed */
+    }
+    </style>
+
+    </head>
+      <body>
+        <link rel="stylesheet" href="css/admin_style.css">
+        <div>
+        <link rel="stylesheet" href="css/admin_main.css">
+
+          <div class="admin-dashboard-container">
+            <div class="admin-dashboard-admin-dashboard">
+              <img
+                alt="Rectangle1553792"
+                src="images/rectangle1553792-c9zi-1100h.png"
+                class="admin-dashboard-rectangle155"
+              />
+              <div class="admin-dashboard-group18">
+                <img
+                  alt="image54619"
+                  src="images/image54619-jbyf-200h.png"
+                  class="admin-dashboard-image5"
+                />
+                <img
+                  alt="spcnremovebgpreview143792"
+                  src="images/spcnremovebgpreview143792-3xcm-200h.png"
+                  class="admin-dashboard-spcnremovebgpreview14"
+                />
+                <img
+                  alt="spcnremovebgpreview154828"
+                  src="images/spcnremovebgpreview154828-1nsl-800h.png"
+                  class="admin-dashboard-spcnremovebgpreview15"
+                />
+                <span class="admin-dashboard-text">
+                  <span>Admin Dashboard</span>
+                </span>
+                <img
+                  alt="image84619"
+                  src="images/image84619-6xw-200h.png"
+                  class="admin-dashboard-image8"
+                />
+                <img
+                  alt="Rectangle1564619"
+                  src="images/rectangle1564619-ke4i.svg"
+                  class="admin-dashboard-rectangle156"
+                />
+                <img
+                  alt="Rectangle1574619"
+                  src="images/rectangle1574619-37nr.svg"
+                  class="admin-dashboard-rectangle157"
+                />
+                <img
+                  alt="Rectangle1584619"
+                  src="images/rectangle1584619-g7.svg"
+                  class="admin-dashboard-rectangle158"
+                />
+                <button class="admin-dashboard-button" onclick="window.location.href = 'students_admin.blade.php'">
+                  <a href="students_admin.blade.php" class="admin-dashboard-text02">STUDENTS</a>
+                </button>
+                <button class="admin-dashboard-button" onclick="window.location.href = 'instructor_admin.blade.php'">
+                  <a href="instructor_admin.blade.php" class="admin-dashboard-text04">INSTRUCTOR</a>
+                </button>
+                <button class="admin-dashboard-button" onclick="window.location.href = 'attendance_log.blade.php'">
+                  <a href="attendance_log.blade.php" class="admin-dashboard-text06">ATTENDANCE LOG</a>
+                </button>
+                <span class="admin-dashboard-text08">
+                  <span>spcregistrar@gmail.com</span>
+                </span>
+                <span class="admin-dashboard-text10">
+                  <span>(+63) 221-6246</span>
+                </span>
+                <img
+                  alt="image64619"
+                  src="images/image64619-yyc-200h.png"
+                  class="admin-dashboard-image6"
+                />
+                <span class="admin-dashboard-text12">
+                  <span>my.spc.edu.ph</span>
+                </span>
+              </div>
+              <div class="admin-dashboard-group17">
+                <img
+                  alt="cba11806"
+                  src="images/cba11806-5q7m-200h.png"
+                  class="admin-dashboard-cba1"
+                />
+                <img
+                  alt="coc11806"
+                  src="images/coc11806-0gp5-200h.png"
+                  class="admin-dashboard-coc1"
+                />
+                <img
+                  alt="cas11806"
+                  src="images/cas11806-f0bi-200w.png"
+                  class="admin-dashboard-cas1"
+                />
+                <img
+                  alt="ccs11806"
+                  src="images/ccs11806-stk-200h.png"
+                  class="admin-dashboard-ccs1"
+                />
+                <img
+                  alt="coe11806"
+                  src="images/coe11806-f82l-200h.png"
+                  class="admin-dashboard-coe1"
+                />
+                <img
+                  alt="ced11806"
+                  src="images/ced11806-h9hn-200w.png"
+                  class="admin-dashboard-ced1"
+                />
+              </div>
+              <img
+                alt="Rectangle1634828"
+                src="images/rectangle1634828-15jt.svg"
+                class="admin-dashboard-rectangle163"
+              />
+              <img
+                alt="Rectangle1644828"
+                src="images/rectangle1644828-t6g.svg"
+                class="admin-dashboard-rectangle164"
+              />
+              <img
+                alt="Rectangle1654828"
+                src="images/rectangle1654828-c9kq.svg"
+                class="admin-dashboard-rectangle165"
+              />
+              <a href="/logout" class="admin-dashboard-text14">Log out</a>
+              <img
+                alt="Rectangle1604828"
+                src="images/rectangle1604828-y2aa.svg"
+                class="admin-dashboard-rectangle160"
+              />
+            </div>
+          </div>
         </div>
-      </a>
-      <div class="v482_8436">
-        <div class="cba-logo"></div>
-        <div class="v180_68"></div>
-        <div class="v180_64"></div>
-        <div class="v180_63"></div>
-        <div class="v180_65"></div>
-        <div class="v180_66"></div>
-      </div>
-      <div class="v482_8444"></div>
-      <div class="v482_8453"></div>
-      <div class="v482_8454"></div>
-      <div class="v482_8455"></div>
-      <a href="logout.php">
-        <button>Log out</button>
-      </a>
-    </div>
-  </body>
-</html>
+      
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      const buttons = document.querySelectorAll('button');
+
+      buttons.forEach(button => {
+        button.addEventListener('mousedown', function() {
+          button.classList.add('button-click-active');
+        });
+
+        button.addEventListener('mouseup', function() {
+          setTimeout(() => {
+            button.classList.remove('button-click-active');
+          }, 200); // Match the duration of the transition
+        });
+      });
+    });
+    </script>
+
+    </body>
+    </html>
+
+
+    <style>
+    /* Dark Mode Styles */
+    body.dark-mode {
+      background-color: #121212;
+      color: #ffffff;
+    }
+    body.dark-mode a {
+      color: #9A9A9A;
+    }
+
+    /* Floating Button Styles */
+    #theme-toggle-btn {
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      background-color: #f0f0f0;
+      color: #000;
+      border: none;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+      cursor: pointer;
+      box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+      font-size: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 1000;
+    }
+    body.dark-mode #theme-toggle-btn {
+      background-color: #333;
+      color: #fff;
+    }
+    </style>
+
+
+    <script>
+    // Dark Mode Toggle Script
+    function toggleDarkLightMode() {
+      var body = document.body;
+      body.classList.toggle('dark-mode');
+
+      var themeIcon = document.getElementById('theme-toggle-icon');
+      if (body.classList.contains('dark-mode')) {
+        themeIcon.innerHTML = '☀️'; // Light Mode Icon
+      } else {
+        themeIcon.innerHTML = '🌙'; // Dark Mode Icon
+      }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var themeToggleBtn = document.createElement('button');
+      themeToggleBtn.id = 'theme-toggle-btn';
+      themeToggleBtn.innerHTML = '<span id="theme-toggle-icon">🌙</span>';
+      themeToggleBtn.onclick = toggleDarkLightMode;
+      document.body.appendChild(themeToggleBtn);
+    });
+    </script>
--- resources/views/student_form.blade.php
+++ resources/views/student_form.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Form</title>
     <link rel="stylesheet" href="https://unpkg.com/unwind-css/dist/unwind.min.css">
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="css/form.css">
 </head>
 <body>
     <div class="container">
