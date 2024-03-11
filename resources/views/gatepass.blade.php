ccc91366c9e7ae107efc3aa5dad92b22b989ae40
Your Name <you@example.com>
2024-03-11 20:13:56 +0800

    Refactor gate pass student confirm CSS styles gatepass1

--- resources/views/gatepass.blade.php
+++ resources/views/gatepass.blade.php
@@ -1,133 +1,159 @@
 <!DOCTYPE html>
 <html lang="english">
-  <head>
-    <title>Gatepass</title>
-    <link rel="icon" type="image/x-icon" href="/images/spc-logo.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta charset="utf-8" />
-    <meta property="twitter:card" content="summary_large_image" />
 
-    <style data-tag="reset-style-sheet">
-      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
-    </style>
-    <style data-tag="default-style-sheet">
-      html {
-        font-family: Inter;
-        font-size: 16px;
-      }
+<head>
+  <title>Gatepass</title>
+  <link rel="icon" type="image/x-icon" href="/images/spc-logo.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta property="twitter:card" content="summary_large_image" />
 
-      body {
-        font-weight: 400;
-        font-style:normal;
-        text-decoration: none;
-        text-transform: none;
-        letter-spacing: normal;
-        line-height: 1.15;
-        color: var(--dl-color-gray-black);
-        background-color: var(--dl-color-gray-white);
+  <style data-tag="reset-style-sheet">
+    html {
+      line-height: 1.15;
+    }
 
-      }
-    </style>
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/animate.css@4.1.1/animate.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/@teleporthq/teleport-custom-scripts/dist/style.css"
-    />
-  </head>
-  <body>
-    <link rel="stylesheet" href="css/gatepass_style.css" />
-    <div>
-      <link rel="stylesheet" href="css/gatepass_main.css" />
+    body {
+      margin: 0;
+    }
 
-      <div class="gate-pass-container">
-        <div class="gate-pass-gate-pass">
-          <img
-            src="images/gatepass/rectangle754132-u5gp-400h.png"
-            alt="Rectangle754132"
-            class="gate-pass-rectangle75"
-          />
-          <img
-            src="images/gatepass/rectangle734132-yqds-200h.png"
-            alt="Rectangle734132"
-            class="gate-pass-rectangle73"
-          />
-          <img
-            src="images/gatepass/rectangle112076-235-800w.png"
-            alt="Rectangle112076"
-            class="gate-pass-rectangle11"
-          />
-          <span class="gate-pass-text">Generate Gate Pass</span>
-          <img
-            src="images/gatepass/rectangle722221-fqoi-400h.png"
-            alt="Rectangle722221"
-            class="gate-pass-rectangle72"
-          />
-          <img
-            src="images/gatepass/rectangle744132-jr4-200h.png"
-            alt="Rectangle744132"
-            class="gate-pass-rectangle74"
-          />
-          <img
-            src="images/gatepass/vector2221-2hxt.svg"
-            alt="Vector2221"
-            class="gate-pass-vector"
-          />
-          <span class="gate-pass-text1"><span>Gate Pass ID</span></span>
-          <span class="gate-pass-text3"><span>Scan First</span></span>
-          <span class="gate-pass-text5">
-            <span>Request Camera Permission</span>
-          </span>
-          <img
-            src="images/gatepass/line204101-l1k.svg"
-            alt="Line204101"
-            class="gate-pass-line20"
-          />
-          <span class="gate-pass-text7"><span>Scan First</span></span>
-          <img
-            src="images/gatepass/spclogo48242-57hg-400h.png"
-            alt="spclogo48242"
-            class="gate-pass-spclogo4"
-          />
-        </div>
+    * {
+      box-sizing: border-box;
+      border-width: 0;
+      border-style: solid;
+    }
+
+    p,
+    li,
+    ul,
+    pre,
+    div,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    figure,
+    blockquote,
+    figcaption {
+      margin: 0;
+      padding: 0;
+    }
+
+    button {
+      background-color: transparent;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      font-family: inherit;
+      font-size: 100%;
+      line-height: 1.15;
+      margin: 0;
+    }
+
+    button,
+    select {
+      text-transform: none;
+    }
+
+    button,
+    [type="button"],
+    [type="reset"],
+    [type="submit"] {
+      -webkit-appearance: button;
+    }
+
+    button::-moz-focus-inner,
+    [type="button"]::-moz-focus-inner,
+    [type="reset"]::-moz-focus-inner,
+    [type="submit"]::-moz-focus-inner {
+      border-style: none;
+      padding: 0;
+    }
+
+    button:-moz-focus,
+    [type="button"]:-moz-focus,
+    [type="reset"]:-moz-focus,
+    [type="submit"]:-moz-focus {
+      outline: 1px dotted ButtonText;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: inherit;
+    }
+
+    input {
+      padding: 2px 4px;
+    }
+
+    img {
+      display: block;
+    }
+
+    html {
+      scroll-behavior: smooth
+    }
+  </style>
+  <style data-tag="default-style-sheet">
+    html {
+      font-family: Inter;
+      font-size: 16px;
+    }
+
+    body {
+      font-weight: 400;
+      font-style: normal;
+      text-decoration: none;
+      text-transform: none;
+      letter-spacing: normal;
+      line-height: 1.15;
+      color: var(--dl-color-gray-black);
+      background-color: var(--dl-color-gray-white);
+
+    }
+  </style>
+  <link rel="stylesheet" href="https://unpkg.com/animate.css@4.1.1/animate.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Goblin+One:wght@400&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://unpkg.com/@teleporthq/teleport-custom-scripts/dist/style.css" />
+</head>
+
+<body>
+  <link rel="stylesheet" href="css/gatepass_style.css" />
+  <div>
+    <link rel="stylesheet" href="css/gatepass_main.css" />
+
+    <div class="gate-pass-container">
+      <div class="gate-pass-gate-pass">
+        <img src="images/gatepass/rectangle754132-u5gp-400h.png" alt="Rectangle754132" class="gate-pass-rectangle75" />
+        <img src="images/gatepass/rectangle734132-yqds-200h.png" alt="Rectangle734132" class="gate-pass-rectangle73" />
+        <img src="images/gatepass/rectangle112076-235-800w.png" alt="Rectangle112076" class="gate-pass-rectangle11" />
+        <span class="gate-pass-text">Generate Gate Pass</span>
+        <img src="images/gatepass/rectangle722221-fqoi-400h.png" alt="Rectangle722221" class="gate-pass-rectangle72" />
+        <img src="images/gatepass/rectangle744132-jr4-200h.png" alt="Rectangle744132" class="gate-pass-rectangle74" />
+        <img src="images/gatepass/vector2221-2hxt.svg" alt="Vector2221" class="gate-pass-vector" />
+        <span class="gate-pass-text1"><span>Gate Pass ID</span></span>
+        <span class="gate-pass-text3"><span>Scan First</span></span>
+        <span class="gate-pass-text5">
+          <span>Request Camera Permission</span>
+        </span>
+        <img src="images/gatepass/line204101-l1k.svg" alt="Line204101" class="gate-pass-line20" />
+        <span class="gate-pass-text7"><span>Scan First</span></span>
+        <img src="images/gatepass/spclogo48242-57hg-400h.png" alt="spclogo48242" class="gate-pass-spclogo4" />
       </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
