abf7d7324b5a18a949fe611438cd6ae3495a76db
Mark Jordan Ugtong <markjordan.ugtong.202200752@gmail.com>
2024-03-18 22:45:23 +0800

    Updated main laravel project

---
+++ database/migrations/2024_02_28_103929_create_roles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('usertype');
+            $table->string('admin');
+            $table->string('faculty');
+            $table->string('students');
+            $table->string('parents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    // Add more plugins here as needed
+  ],
+};
